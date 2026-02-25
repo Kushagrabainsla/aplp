da41e0d260664b6d870728cabd72ba4ab2589376
Kushagra Bainsla <kushagrabainsla@gmail.com>
2026-02-24 20:29:28 -0800

    Homework2

--- haskell/homework2/WhileInterp.hs
+++ haskell/homework2/WhileInterp.hs
@@ -44,6 +44,9 @@ data Binop =
   | Ge       -- >= :: Int -> Int -> Bool
   | Lt       -- <  :: Int -> Int -> Bool
   | Le       -- <= :: Int -> Int -> Bool
+  | And      -- and :: Bool -> Bool -> Bool
+  | Or       -- or :: Bool -> Bool -> Bool
+  | Not      -- not :: Bool -> Bool -> Bool (unary, second arg ignored)
   deriving (Show)
 
 data Value =
@@ -52,20 +55,73 @@ data Value =
   deriving (Show)
 
 
--- This function will be useful for defining binary operations.
--- The first case is done for you.
--- Be sure to explicitly check for a divide by 0 and throw an error.
 applyOp :: Binop -> Value -> Value -> Value
 applyOp Plus (IntVal i) (IntVal j) = IntVal $ i + j
-applyOp _ _ _ = error "TBD"
+applyOp Minus (IntVal i) (IntVal j) = IntVal $ i - j
+applyOp Times (IntVal i) (IntVal j) = IntVal $ i * j
+applyOp Divide (IntVal i) (IntVal 0) = error "Divide by zero"
+applyOp Divide (IntVal i) (IntVal j) = IntVal $ i `div` j
+applyOp Gt (IntVal i) (IntVal j) = BoolVal $ i > j
+applyOp Ge (IntVal i) (IntVal j) = BoolVal $ i >= j
+applyOp Lt (IntVal i) (IntVal j) = BoolVal $ i < j
+applyOp Le (IntVal i) (IntVal j) = BoolVal $ i <= j
+-- Boolean operators
+applyOp And (BoolVal b1) (BoolVal b2) = BoolVal $ b1 && b2
+applyOp Or (BoolVal b1) (BoolVal b2) = BoolVal $ b1 || b2
+applyOp _ _ _ = error "Invalid operands for operator"
 
 
 -- Implement this function according to the specified semantics
 evaluate :: Expression -> Store -> (Value, Store)
+evaluate (Var x) s = case Map.lookup x s of
+  Just v  -> (v, s)
+  Nothing -> error $ "Unbound variable: " ++ x
+evaluate (Val v) s = (v, s)
+evaluate (Assign x e) s =
+  let (v, s') = evaluate e s
+  in (v, Map.insert x v s')
+evaluate (Sequence e1 e2) s =
+  let (_, s')   = evaluate e1 s
+      (v2, s'') = evaluate e2 s'
+  in (v2, s'')
+-- Boolean short-circuit ops must come BEFORE the generic Op case
+evaluate (Op And e1 e2) s =
+  let (v1, s1) = evaluate e1 s
+  in case v1 of
+    BoolVal True  -> evaluate e2 s1
+    BoolVal False -> (BoolVal False, s1)
+    _             -> error "Operand for And must be boolean"
+evaluate (Op Or e1 e2) s =
+  let (v1, s1) = evaluate e1 s
+  in case v1 of
+    BoolVal True  -> (BoolVal True, s1)
+    BoolVal False -> evaluate e2 s1
+    _             -> error "Operand for Or must be boolean"
+evaluate (Op Not e1 _) s =
+  let (v1, s1) = evaluate e1 s
+  in case v1 of
+    BoolVal True  -> (BoolVal False, s1)
+    BoolVal False -> (BoolVal True, s1)
+    _             -> error "Operand for Not must be boolean"
+-- Generic binary op (arithmetic and comparisons)
 evaluate (Op o e1 e2) s =
-  let (v1,s1) = evaluate e1 s
-      (v2,s') = evaluate e2 s1
+  let (v1, s1) = evaluate e1 s
+      (v2, s') = evaluate e2 s1
   in (applyOp o v1 v2, s')
+evaluate (If e1 e2 e3) s =
+  let (v1, s1) = evaluate e1 s
+  in case v1 of
+    BoolVal True  -> let (v2, s2) = evaluate e2 s1 in (v2, s2)
+    BoolVal False -> let (v3, s3) = evaluate e3 s1 in (v3, s3)
+    _             -> error "Condition in If must be boolean"
+evaluate (While e1 e2) s =
+  let (v1, s1) = evaluate e1 s
+  in case v1 of
+    BoolVal True  -> let (_, s2) = evaluate e2 s1
+                         (v, s3) = evaluate (While e1 e2) s2
+                     in (v, s3)
+    BoolVal False -> (BoolVal False, s1)
+    _             -> error "Condition in While must be boolean"
 evaluate _ _ = error "TBD"
 
 
